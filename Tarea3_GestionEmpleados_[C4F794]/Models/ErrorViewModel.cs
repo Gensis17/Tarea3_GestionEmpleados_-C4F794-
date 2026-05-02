e9ff61233db43986269ec3d4862cb1f13ad06b45
Genesis Gutierrez <Gggensis2005@gmail.com>
2026-05-01 19:16:57 -0600

    Agregar archivos de proyecto.

---
+++ Tarea3_GestionEmpleados_[C4F794]/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarea3_GestionEmpleados__C4F794_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
