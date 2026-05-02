a4c67206051413a9eb6198c422deee8f72a8774c
Genesis Gutierrez <Gggensis2005@gmail.com>
2026-05-02 00:06:24 -0600

    Implementación y configuración de AppDbContext con Entity Framework Core

---
+++ Tarea3_GestionEmpleados_[C4F794]/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Tarea3_GestionEmpleados__C4F794_.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tarea3_GestionEmpleados__C4F794_.Data
+
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
+
+        // 👇 AQUÍ VA
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Empleado>()
+                .Property(e => e.Salario)
+                .HasPrecision(18, 2);
+        }
+    }
+}
