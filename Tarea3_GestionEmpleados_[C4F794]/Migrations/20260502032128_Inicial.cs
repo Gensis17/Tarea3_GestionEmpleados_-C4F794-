0cedc47504a1d00c020ea2f30d1131e5cbb1e251
Genesis Gutierrez <Gggensis2005@gmail.com>
2026-05-02 00:11:08 -0600

    Agregar migraciones de Entity Framework Core

---
+++ Tarea3_GestionEmpleados_[C4F794]/Migrations/20260502032128_Inicial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tarea3_GestionEmpleados__C4F794_.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
