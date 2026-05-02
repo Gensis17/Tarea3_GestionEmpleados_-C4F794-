71f5999db50d827daa925b3793f0b0ad701aaf51
Genesis Gutierrez <Gggensis2005@gmail.com>
2026-05-02 00:07:12 -0600

    Agregar modelo Empleado en la carpeta Models

---
+++ Tarea3_GestionEmpleados_[C4F794]/Models/Empleado.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tarea3_GestionEmpleados__C4F794_.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Apellidos { get; set; }
+
+        [Required]
+        public string Departamento { get; set; }
+
+        [Required]
+        [Range(400000, 10000000)]
+        public decimal Salario { get; set; }
+
+        public DateTime FechaIngreso { get; set; }
+
+        public bool Activo { get; set; }
+
+        public string NombreCompleto => $"{Nombre} {Apellidos}";
+    }
+}
