36c1cb0858754a05403d1278f36483d87422b0c7
Genesis Gutierrez <Gggensis2005@gmail.com>
2026-05-02 00:58:47 -0600

    Actualizar configuracion de inicio en Program.cs

--- Tarea3_GestionEmpleados_[C4F794]/Program.cs
+++ Tarea3_GestionEmpleados_[C4F794]/Program.cs
@@ -1,7 +1,16 @@
+using Tarea3_GestionEmpleados__C4F794_.Data;
+using Tarea3_GestionEmpleados__C4F794_.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IEmpleadoRepository, EmpleadoRepository>();  
 
 var app = builder.Build();
 
@@ -22,7 +31,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Empleados}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
